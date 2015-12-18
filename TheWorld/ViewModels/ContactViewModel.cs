c66ce9ccfcd3de100b15907a6da39e4bf2d5287d
Interview <Interview>
2015-12-18 05:18:03 -0600

    Initial Commit, adding project

---
+++ TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+      
+        public string Name { get; set; }
+       
+        public string Email { get; set; }
+      
+        public string Message { get; set; }
+    }
+}
